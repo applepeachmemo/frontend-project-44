30740b614b72d4ccd6f0c3b02cedb8b93466f597
lena <lena@MacBook-Air-Alexandr.local>
2023-06-01 17:33:56 +0400

    add src/index.js

--- src/index.js
+++ src/index.js
@@ -2,26 +2,31 @@ import readlineSync from 'readline-sync';
 
 const roundsCount = 3;
 
-export default (description, getRound) => {
+export default (description, getround) => {
   console.log('Welcome to the Brain Games!');
   const playerName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${playerName}!`);
 
   console.log(description);
 
+  let hasWrongAnswer = false;
+
   for (let i = 0; i < roundsCount; i += 1) {
     const [question, correctAnswer] = getRound();
     console.log(`Question: ${question}`);
     const playerAnswer = readlineSync.question('You answer: ');
 
-    if (correctAnswer === playerAnswer) {
-      console.log('Correct!');
-      
-    } 
-    console.log(`'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-    console.log(`Let's try again, ${playerName}`);
-    return;
-    
+    if (correctAnswer !== playerAnswer) {
+      console.log(`'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${playerName}`);
+      hasWrongAnswer = true;
+      break;
+    }
+
+    console.log('Correct!');
   }
-  console.log(`Congratulations, ${playerName}`);
+
+  if (!hasWrongAnswer) {
+    console.log(`Congratulations, ${playerName}`);
+  }
 };
