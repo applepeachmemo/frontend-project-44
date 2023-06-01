9993a48185d9d2770c2ec8965f165f2eb7446158
lena <lena@MacBook-Air-Alexandr.local>
2023-06-01 17:37:27 +0400

    add src/index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const roundsCount = 3;
 
-export default (description, getround) => {
+export default (description, getRound) => {
   console.log('Welcome to the Brain Games!');
   const playerName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${playerName}!`);
