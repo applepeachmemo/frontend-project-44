cd2ff212804a81c014bfe50f47c34e3b72350067
lena <lena@MacBook-Air-Alexandr.local>
2023-06-02 17:10:12 +0400

    add src/utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,3 @@
 export const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-export const isEvenNumber = (number) => number % 2 === 0;
+export const isEven = (number) => number % 2 === 0;
