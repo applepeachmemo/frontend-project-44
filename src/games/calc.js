b295089036ef503620bafdde58d4593c544941d8
lena <lena@MacBook-Air-Alexandr.local>
2023-06-13 15:03:35 +0400

    add src/games/calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -4,13 +4,6 @@ import { getRandomNumber } from '../utils.js';
 const operationSymbols = ['+', '-', '*'];
 const description = 'What is the result of the expression?';
 
-const getRandomOperationSymbol = (symbols) => {
-  const symbolsLength = symbols.length;
-  const randomIndex = getRandomNumber(0, symbolsLength - 1);
-  const randomSymbol = symbols[randomIndex];
-  return randomSymbol;
-};
-
 const calculate = (symbol, x, y) => {
   switch (symbol) {
     case '+':
@@ -25,20 +18,14 @@ const calculate = (symbol, x, y) => {
 };
 
 const getRound = () => {
-  const randomSymbol = getRandomOperationSymbol(operationSymbols);
+  const symbol = operationSymbols[getRandomNumber(0, operationSymbols.length - 1)];
   const number1 = getRandomNumber(0, 10);
   const number2 = getRandomNumber(0, 10);
 
-  const question = `${number1} ${randomSymbol} ${number2}`;
-  const correctAnswer = String(calculate(randomSymbol, number1, number2));
+  const question = `${number1} ${symbol} ${number2}`;
+  const correctAnswer = String(calculate(symbol, number1, number2));
 
   return [question, correctAnswer];
 };
 
-export default () => {
-  try {
-    runGame(description, getRound);
-  } catch (error) {
-    console.error('An error occurred:', error.message);
-  }
-};
+runGame(description, getRound);
