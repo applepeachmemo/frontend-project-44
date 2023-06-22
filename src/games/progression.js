edf6f7dbef1639f4b0c0d7c67fd3af7288a7ff3f
lena <lena@MacBook-Air-Alexandr.local>
2023-06-22 14:42:52 +0400

    add src/games/progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,25 +1,27 @@
 import runGame from '../index.js';
 import { getRandomNumber } from '../utils.js';
 
-const progressionLength = 10;
+
 const description = 'What number is missing in the progression?';
 
-const getArithmeticProgression = () => {
-  let progressionNumber = getRandomNumber(0, 50);
-  const step = getRandomNumber(2, 5);
+const getArithmeticProgression = (start, step, length) => {
   const progression = [];
 
-  for (let i = 0; i < progressionLength; i += 1) {
-    progression.push(progressionNumber);
-    progressionNumber += step;
+  for (let i = 0; i < length; i += 1) {
+    const currentValue = start + step * i;
+    progression.push(currentValue);
   }
 
   return progression;
 };
 
 const getRound = () => {
-  const progression = getArithmeticProgression();
-  const randomIndex = getRandomNumber(0, progressionLength - 1);
+  const start = getRandomNumber(0, 50);
+  const step = getRandomNumber(2, 5);
+  const length = 10;
+
+  const progression = getArithmeticProgression(start, step, length);
+  const randomIndex = getRandomNumber(0, progression.length - 1);
 
   const correctAnswer = String(progression[randomIndex]);
   progression[randomIndex] = '..';
